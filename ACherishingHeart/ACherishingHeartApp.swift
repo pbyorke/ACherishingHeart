987135ed80ce815f672e3aeea7c515a4215577f3
Pete Yorke <pbyorke@gmail.com>
2021-07-21 14:43:16 -0400

    Initial Commit

---
+++ ACherishingHeart/ACherishingHeartApp.swift
@@ -0,0 +1,17 @@
+//
+//  ACherishingHeartApp.swift
+//  ACherishingHeart
+//
+//  Created by Pete Yorke on 7/21/21.
+//
+
+import SwiftUI
+
+@main
+struct ACherishingHeartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ACherishingHeart/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ACherishingHeart
+//
+//  Created by Pete Yorke on 7/21/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
